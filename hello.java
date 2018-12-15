d3d033c19987c687681b04c2fe2513d3ab8c6e0e
hiteshaddanki <hiteshaddanki1@gmail.com>
2018-12-15 13:17:29 +0530

    'first'

---
+++ hello.java
@@ -0,0 +1,20 @@
+public class hello{
+
+private int id;
+private String name;
+
+hello(int id,String name){
+this.id=id;
+this.name=name;
+}
+
+public void getdata(){
+
+	System.out.println(id+""+name);
+}
+public static void main(String []s){
+
+	hello h=new hello(1,"hitesh");
+	h.getdata();
+}
+}
